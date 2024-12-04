aa5f35eeecc5a62eb46462f293851352bbc228a4
baptdes <desnouck.baptiste@gmail.com>
2024-12-04 10:25:14 +0100

    TD RatToTam finished

--- passeCodeRatToTam.ml
+++ passeCodeRatToTam.ml
@@ -6,7 +6,25 @@ type t1 = Ast.AstTypePlacement.programme
 type t2 = string
 
 (*AstPlacement.expression -> string *)
-let rec analyse_code_expression e = failwith "todo"
+let rec analyse_code_expression e = match e with
+  | AppelFonction (id,le) -> failwith "todo"
+  | Ident (info) -> 
+    begin
+      match info_ast_to_info info with
+        | InfoVar(_,t,dep,reg) -> Tam.load (getTaille t) dep reg
+        | InfoConst(_,c) -> Tam.loadl_int c
+        | _ -> failwith "Problème dans la passe Tds"
+    end
+  | Booleen b -> failwith "todo"
+  | Entier i -> failwith "todo"
+  | Unaire (op,e) -> 
+    let ne = analyse_code_expression e1 in
+    begin
+      match op with
+        | Numerateur -> ne ^ Tam.pop(0) 1
+        | Denominateur -> ne ^ Tam.pop(1) 1
+    end
+  | Binaire (op,e1,e2) -> failwith "todo"
 
 (* AstPlacement.instruction -> String *)
 let rec analyse_code_instruction i = match i with
